cf024571fb859ed94ca94fe297d669d9bc732c5b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-01-14 12:59:20 -0500

    added path to ligand/receptor files

--- models/test_models.py
+++ models/test_models.py
@@ -8,4 +8,7 @@ receptor = models.Molecule(geometry=numpy.random.rand(natoms,3) * 10, symbols=['
 
 docking_input = input.DockingInput(Ligand=ligand, Receptor=receptor)
 
-docking_input_data = input.DockingInputData(LigandPath='.', ReceptorPath='.')
+docking_input_data = input.DockingInputData(
+					LigandPath='../data/PHIPA_C2/fragments_screened.csv', 
+					ReceptorPath='../data/PHIPA_C2/PHIPA_C2_apo.pdb'z
+					)
