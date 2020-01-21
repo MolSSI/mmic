938e49bfa12c690a30917f68942ad15be58f3602
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-01-20 19:45:09 -0500

    replaced OpenBabelOutput with CmdOutput

---
+++ models/output.py
@@ -0,0 +1,15 @@
+from qcelemental import models
+from typing import List, Optional, Tuple
+from .input import DockingInput
+
+class DockingOutput(models.ProtoModel):
+    Docking_Input: DockingInput
+    Poses: List[Tuple[models.Molecule, models.Molecule]]
+    Scores: Optional[List[float]] = None
+
+class Affinity(models.ProtoModel):
+    Docking_Output: DockingOutput
+    Affinity: float
+
+class CmdOutput(models.ProtoModel):
+    FileContents: str
