49a8623a3d9a4f3904fb5b3cfb31e017d9da934b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-01-20 19:42:58 -0500

    added CmdInput class

--- models/input.py
+++ models/input.py
@@ -4,14 +4,20 @@ from typing import List, Optional, Tuple, Union
 class DockingInputData(models.ProtoModel):
     Ligand: Union[models.Molecule, str]
     Receptor: Union[models.Molecule, str]
-    BindingSitePath: Optional[str] = None
+    BindingSite: Optional[Union[List[Tuple[float, float, float]], str]] = None
 
 class DockingInput(models.ProtoModel):
     Ligand: models.Molecule
     Receptor: models.Molecule
     BindingSite: Optional[List[Tuple[float]]] = None
 
-class OpenBabelInput(models.ProtoModel):
+class CmdInput(models.ProtoModel):
     Input: str
-    Output: str
     Args: Optional[List[str]]
+
+class OpenBabelInput(CmdInput):
+    OutputExt: str
+    InputExt: Optional[str]
+    
+class GrepInput(CmdInput):
+    Pattern: str
