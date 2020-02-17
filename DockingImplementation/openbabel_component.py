a2282aff50a8d4341a22a8bcc17b536b9ef33e6f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-17 01:35:08 -0500

    Obabel component works with boilerplate

--- DockingImplementation/openbabel_component.py
+++ DockingImplementation/openbabel_component.py
@@ -11,37 +11,83 @@ from models.output import FileOutput
 
 class OpenBabel(ProgramHarness):
 
-    _defaults = {
-        "name": "OpenBabel",
-        "scratch": False,
-        "thread_safe": True,
-        "thread_parallel": False,
-        "node_parallel": False,
-        "managed_memory": True,
-    }
+    @classmethod
+    def input(cls):
+        return OpenBabelInput
 
     @classmethod
-    def compute(cls, input_data: OpenBabelInput, config: "TaskConfig" = None) -> FileOutput:
-        inp = input_data.Input
+    def output(cls):
+        return FileOutput
+
+    def execute(self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None,) -> Tuple[bool, Dict[str, Any]]:
+
+        inp = inputs.Input
         inp_ext = inp.split('.')[-1]
 
-        outp_ext = input_data.OutputExt
+        outp_ext = inputs.OutputExt
 
-        args = input_data.Args
+        args = inputs.Args
 
         input_model = {'input': inp, 'input_ext': inp_ext, 'output': 'tmp.' + outp_ext, 'output_ext': outp_ext, 'args': args}
 
-        execute_input = cls.build_input(input_model, config)
-        exe_success, proc = cls.execute(execute_input)
+        execute_input = self.build_input(input_model)
+        exe_success, proc = self.run(execute_input)
 
         if exe_success:
-            return cls.parse_output(proc['outfiles'], input_model)
+            return True, self.parse_output(proc['outfiles'], input_model)
         else:
-            raise ValueError(proc["stderr"])
+            raise ValueError(proc["stderr"]) 
 
-    @classmethod
+    def found(raise_error: bool = False) -> bool:
+        """
+        Checks if the program can be found.
+        Parameters
+        ----------
+        raise_error : bool, optional
+            If True, raises an error if the program cannot be found.
+        Returns
+        -------
+        bool
+            Returns True if the program was found, False otherwise.
+        """
+
+    def run(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None) -> Tuple[bool, Dict[str, Any]]:
+
+        infiles = inputs["infiles"]
+
+        outfiles = inputs["outfiles"]
+        if extra_outfiles is not None:
+            outfiles.extend(extra_outfiles)
+
+        command = inputs["command"]
+        if extra_commands is not None:
+            command.extend(extra_commands)
+
+        exe_success, proc = execute(
+            command,
+            infiles=infiles,
+            outfiles=outfiles,
+            scratch_directory=inputs["scratch_directory"],
+            scratch_name=scratch_name,
+            timeout=timeout,
+            environment=inputs.get("environment", None),
+        )
+        
+        return exe_success, proc
+
     def build_input(
-        cls, input_model: Dict[str, Any], config: "TaskConfig" = None, template: Optional[str] = None
+        self, input_model: Dict[str, Any], config: "TaskConfig" = None, template: Optional[str] = None
     ) -> Dict[str, Any]:
         
         cmd = ["obabel", "-i{}".format(input_model['input_ext']), input_model['input'], \
@@ -67,39 +113,7 @@ class OpenBabel(ProgramHarness):
             "environment": env
         }
 
-    @classmethod
-    def execute(
-        cls,
-        inputs: Dict[str, Any],
-        extra_outfiles: Optional[List[str]] = None,
-        extra_commands: Optional[List[str]] = None,
-        scratch_name: Optional[str] = None,
-        timeout: Optional[int] = None,
-    ) -> Tuple[bool, Dict[str, Any]]:
-
-        infiles = inputs["infiles"]
-
-        outfiles = inputs["outfiles"]
-        if extra_outfiles is not None:
-            outfiles.extend(extra_outfiles)
-
-        command = inputs["command"]
-        if extra_commands is not None:
-            command.extend(extra_commands)
-
-        exe_success, proc = execute(
-            command,
-            infiles=infiles,
-            outfiles=outfiles,
-            scratch_directory=inputs["scratch_directory"],
-            scratch_name=scratch_name,
-            timeout=timeout,
-            environment=inputs.get("environment", None),
-        )
-        return exe_success, proc
-
-    @classmethod
-    def parse_output(cls, outfiles: Dict[str, str], input_model: Dict[str, Any]) -> FileOutput:
+    def parse_output(self, outfiles: Dict[str, str], input_model: Dict[str, Any]) -> FileOutput:
         
         output_file = outfiles[input_model['output']]
 
