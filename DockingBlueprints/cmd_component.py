bcc9d1bb67d45ac3e6bda12d3239140723e97d2d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-18 12:48:19 -0500

    added CmdComponent class

---
+++ DockingBlueprints/cmd_component.py
@@ -0,0 +1,81 @@
+import sys
+sys.path.insert(0, '..')
+
+from qcengine.util import execute
+from qcelemental import models
+from base_component.base_component import ProgramHarness
+from typing import Any, Dict, List, Optional, Tuple
+import os
+from qcelemental import models
+
+
+class CmdComponent(ProgramHarness):
+
+    @classmethod
+    def input(cls):
+        return models.ProtoModel
+
+    @classmethod
+    def output(cls):
+        return models.ProtoModel
+
+    def found(raise_error: bool = False) -> bool:
+        """
+        Checks if the program can be found.
+        Parameters
+        ----------
+        raise_error : bool, optional
+            If True, raises an error if the program cannot be found.
+        Returns
+        -------
+        bool
+            Returns True if the program was found, False otherwise.
+        """
+
+    def build_input(
+        self, input_model: "DockingInputData", config: "TaskConfig" = None, template: Optional[str] = None
+    ) -> Dict[str, Any]:
+        raise ValueError("build_input is not implemented for {}.", self.__class__)
+
+    def execute(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None,
+    ) -> Tuple[bool, Dict[str, Any]]:
+        raise ValueError("execute is not implemented for {}.", self.__class__)
+
+    def run(
+        self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None) -> Tuple[bool, Dict[str, Any]]:
+
+        infiles = inputs["infiles"]
+
+        outfiles = inputs["outfiles"]
+        if extra_outfiles is not None:
+            outfiles.extend(extra_outfiles)
+
+        command = inputs["command"]
+        if extra_commands is not None:
+            command.extend(extra_commands)
+
+        exe_success, proc = execute(
+            command,
+            infiles=infiles,
+            outfiles=outfiles,
+            scratch_directory=inputs["scratch_directory"],
+            scratch_name=scratch_name,
+            timeout=timeout,
+            environment=inputs.get("environment", None)
+        )
+        
+        return exe_success, proc
+
+    def parse_output(self, outfiles: Dict[str, str], input_model: "DockingInputData") -> "DockingInput":
+        raise ValueError("parse_output is not implemented for {}.", self.__class__)
