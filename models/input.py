e6b9e25aedf91f8a65f601f3d76a4919ae2faa07
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-18 22:09:19 -0500

    all optional attrs set to None; completed DockingSimInput definition; simplified OpenBabelInput

--- models/input.py
+++ models/input.py
@@ -5,25 +5,27 @@ from models.molecule import MMolecule
 class DockingInputData(models.ProtoModel):
     Ligand: Union[MMolecule, str]
     Receptor: Union[MMolecule, str]
-    BindingSite: Optional[Union[List[Tuple[float, float, float]], str]]
+    BindingSite: Optional[Union[List[Tuple[float, float, float]], str]] = None
 
 class DockingInput(models.ProtoModel):
     Ligand: MMolecule
     Receptor: MMolecule
-    BindingSite: Optional[List[Tuple[float]]]
+    BindingSite: Optional[List[Tuple[float]]] = None
 
 class CmdInput(models.ProtoModel):
     Input: Union[str, List[str]]
-    Args: Optional[List[str]]
+    Output: Optional[str] = None
+    Args: Optional[List[str]] = None
 
 class OpenBabelInput(CmdInput):
     OutputExt: str
-    InputExt: Optional[str]
     
 class GrepInput(CmdInput):
     Pattern: str
 
 class DockingSimInput(models.ProtoModel):
-	Exhaustiveness: Optional[int] = None
-	Seed: Optional[int] = None
-	SearchSpace: Optional[Tuple[float, float, float, float, float, float]] = None
+    Ligand: Union[str, MMolecule]
+    Receptor: Union[str, MMolecule]
+    Exhaustiveness: Optional[int] = None
+    Seed: Optional[int] = None
+    SearchSpace: Optional[Tuple[float, float, float, float, float, float]] = None
