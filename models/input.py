35f007be25949b2d046415f004db77c6d9010076
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-19 00:54:27 -0500

    updated schema for DockingSimInput

--- models/input.py
+++ models/input.py
@@ -6,11 +6,13 @@ class DockingInputData(models.ProtoModel):
     Ligand: Union[MMolecule, str]
     Receptor: Union[MMolecule, str]
     BindingSite: Optional[Union[List[Tuple[float, float, float]], str]] = None
+    Flexible: Optional[Union[MMolecule, str]] = None
 
 class DockingInput(models.ProtoModel):
     Ligand: MMolecule
     Receptor: MMolecule
     BindingSite: Optional[List[Tuple[float]]] = None
+    Flexible: Optional[MMolecule] = None
 
 class CmdInput(models.ProtoModel):
     Input: Union[str, List[str]]
@@ -24,8 +26,16 @@ class GrepInput(CmdInput):
     Pattern: str
 
 class DockingSimInput(models.ProtoModel):
+    # Basic attr
     Ligand: Union[str, MMolecule]
     Receptor: Union[str, MMolecule]
-    Exhaustiveness: Optional[int] = None
+
+    # Sim params
+    Exhaustiveness: Optional[int] = 8
     Seed: Optional[int] = None
+    BindingModes: Optional[int] = 9
+    EnergyRange: Optional[int] = 3 # in kcal/mol
+    NumProc: Optional[int] = None
     SearchSpace: Optional[Tuple[float, float, float, float, float, float]] = None
+
+    # Flexible substructures (e.g. side chains)
