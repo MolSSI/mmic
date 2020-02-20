4efb6f4a6e4dc3308b548f7a98e130fcd2d2aabe
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-20 00:57:48 -0500

    added test files for autodock vina

---
+++ data/autodock_test/run.sh
@@ -0,0 +1,5 @@
+vina --center_x 11 --center_y 90.5 --center_z 57.5 \
+     --size_x 22 --size_y 24 --size_z 28 \
+     --receptor input/receptor.pdbqt --ligand input/ligand.pdbqt \
+     --exhaustiveness 8 \
+     --log vina.log
