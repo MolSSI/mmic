7b6b1e0b2a47260ca960594c54c53c3cf8cf3b97
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-22 15:50:20 -0500

    test works for refactored code

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,26 +1,27 @@
 import sys
-
 sys.path.insert(0, '..')
 
-from models import input
+from models.components.docking.input import DockingPrepInput
+from models.components.utils.input import OpenBabelInput
+from models.components.utils.input import GrepInput
 import numpy
 import os
 
 # Test MMolecule
-from models.molecule import MMolecule
+from models.domains.docking.molecule import MMolecule
 
 # Test input file
-pdb_file = os.path.abspath('data/dialanine/dialanine.pdb')
+pdb_file = os.path.abspath('../data/dialanine/dialanine.pdb')
 
-docking_input_data = input.DockingInputData(
+docking_input_data = DockingPrepInput(
 			Ligand='CCC',
 			Receptor=pdb_file
 		)
 
-from DockingImplementation.openbabel_component import OpenBabel
-from DockingImplementation.grep_component import Grep
-from DockingImplementation.autodock_prep_component import AutoDockPrep
-from DockingImplementation.input_prep_component import MolSSIInputPrep
+from components.implementation.openbabel_component import OpenBabel
+from components.implementation.grep_component import Grep
+from components.implementation.autodock_prep_component import AutoDockPrep
+from components.implementation.input_prep_component import MolSSIInputPrep
 
 from config import TaskConfig
 
@@ -29,18 +30,21 @@ from config import TaskConfig
 docking_input = MolSSIInputPrep.compute(docking_input_data)
 
 # Test for openbabel
-obabel_input = input.OpenBabelInput(Input=pdb_file, OutputExt='pdbqt')
+obabel_input = OpenBabelInput(Input=pdb_file, OutputExt='pdbqt')
 obabel_output = OpenBabel.compute(obabel_input)
-
-with open('test.pdbqt', 'w') as fp:
-	fp.write(obabel_output.Contents)
+print("==============================")
+print("OBABEL OUTPUT:")
+print("==============================")
+print(obabel_output.Contents)
 
 # Test for grep
-grep_input = input.GrepInput(Input=pdb_file, Pattern='ATOM')
+grep_input = GrepInput(Input=pdb_file, Pattern='ATOM')
 grep_output = Grep.compute(grep_input)
-
-with open('test.grep', 'w') as fp:
-	fp.write(grep_output.Contents)
+print("==============================")
+print("GREP OUTPUT:")
+print("==============================")
+print(grep_output.Contents)
 
 # Test for AutodockPrep
-ADP = AutoDockPrep.compute(input_data=docking_input)
+# ADP = AutoDockPrep.compute(input_data=docking_input)
+
