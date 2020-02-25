6a7b8635edc9cacfbdd9a3f8c79af6d5ca903e61
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-02-25 01:35:44 -0500

    completed basic definition of classical MM molecule

--- models/domains/classmech/molecule.py
+++ models/domains/classmech/molecule.py
@@ -1,11 +1,35 @@
-from typing import List, Optional, Union
 from qcelemental import models
-from pydantic import validator
+from typing import List, Optional, Any, Dict
 import os
+from rdkit import Chem
+from pydantic import validator
+
+from rdkit import rdBase
+rdBase.DisableLog('rdApp.error')
+
+
+class _codesSupported:
+    codes = ('Smiles', 'Smarts', 'Inchi', 'FASTA', 'HELM', 'Sequence')
 
 class ChemCode(models.ProtoModel):
     code: str
 
     @validator('code')
     def validCode(cls, v):
+        for code in _codesSupported.codes:
+            function = getattr(Chem, f"MolFrom{code}")(v)
+            if function:
+                break
         return v
+
+class Identifiers(models.molecule.Identifiers):
+    smarts: Optional[ChemCode] = None
+    sequence: Optional[ChemCode] = None
+    fasta: Optional[ChemCode] = None
+    helm: Optional[ChemCode] = None
+
+class MMolecule(models.Molecule):
+    residues: Optional[Dict[str, List[int]]] = None
+    chains: Optional[Dict[str, List[int]]] = None
+    segments: Optional[Dict[str, List[int]]] = None
+    identifiers: Optional[Identifiers] = None
