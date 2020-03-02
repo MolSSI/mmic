6276b6a9dd6ac8a1a43299ba79d47ffb29e74ddb
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-02 18:03:07 -0500

    changed attrs starting letter to lowercase

--- models/components/docking/output.py
+++ models/components/docking/output.py
@@ -1,13 +1,13 @@
 from typing import List, Optional, Tuple, Union
 from .input import DockingInput
-from models.molecule import MMolecule
+from models.domains.docking.molecule import MMolecule
 from qcelemental import models
 
 class DockingOutput(models.ProtoModel):
-    Docking_Input: DockingInput
-    Poses: List[MMolecule]
-    Flexible: Optional[List[MMolecule]] = None
-    Scores: Optional[List[float]] = None
+    dockingInput: DockingInput
+    poses: List[MMolecule]
+    flexible: Optional[List[MMolecule]] = None
+    scores: Optional[List[float]] = None
 
 class DockingSimOutput(models.ProtoModel):
     pass
