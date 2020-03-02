206b3cd5dc9ea9922ca08287afbca2b76d85496b
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-02 18:02:30 -0500

    fixed typo: AutoDickSimOutput

--- models/components/docking/autodock/output.py
+++ models/components/docking/autodock/output.py
@@ -1,17 +1,17 @@
 from ..input import DockingInput
 from ..output import DockingSimOutput
+from models.domains.docking.molecule import MMolecule
 
 from qcelemental import models
 from typing import List, Optional, Tuple, Union
 
 class AutoDockPostOutput(DockingInput):
-	Docking_Input: DockingInput
+    Docking_Input: DockingInput
     Ligand: Union[MMolecule, str]
     Receptor: Union[MMolecule, str]
     Scores: List[float]
-    Poses: List[Union[MMolecule, str]]    
+    Poses: List[Union[MMolecule, str]]
     Flexible: Optional[Union[MMolecule, str]] = None
 
-class AutoDickSimOutput(DockingSimOutput):
-	pass
-    
+class AutoDockSimOutput(DockingSimOutput):
+    pass
