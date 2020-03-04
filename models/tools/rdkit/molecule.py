d62df281fa9e68c8f8f48a4acd7ce421347af7c1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-03 22:23:25 -0500

    created Bond and MMToRDKit classes

--- models/tools/rdkit/molecule.py
+++ models/tools/rdkit/molecule.py
@@ -5,8 +5,55 @@ try:
 except:
     raise ModuleNotFoundError('Make sure rdkit is installed for code validation.')
 
+class Bond:
+    """ RDKit-based bond order: {0: unspecified, 1: single, etc., up to 21} """
+    try:
+        from rdkit import Chem
+    except:
+        raise ModuleNotFoundError('Make sure rdkit is installed for code validation.')    
+    orders = list(Chem.BondType.values.values())
+
 class RDKitMolecule(models.ProtoModel):
     mol: Chem.rdchem.Mol = None
 
     class Config:
         arbitrary_types_allowed = True
+
+class MMToRDKit:
+    try:
+        from models.tools.rdkit.molecule import RDKitMolecule
+    except:
+        raise ModuleNotFoundError('Make sure rdkit is installed for code validation.')
+
+    @staticmethod
+    def convert(mmol: "MMolecule") -> RDKitMolecule:
+        try:
+            from rdkit import Chem
+        except:
+            raise ModuleNotFoundError('Make sure rdkit is installed for code validation.')
+        
+        rdkmol = Chem.Mol()
+        erdkmol = Chem.EditableMol(rdkmol)
+
+        for index, symb in enumerate(mmol.symbols):
+            atom = Chem.Atom(symb)
+            resname, resnum = mmol.residues[index]
+            residue = Chem.AtomPDBResidueInfo()
+            residue.SetResidueName(resname)
+            residue.SetResidueNumber(resnum)
+            residue.SetOccupancy(1.0)
+            residue.SetTempFactor(0.0)
+            atom.SetMonomerInfo(residue)
+            #atom.SetIdx(index+1)
+            erdkmol.AddAtom(atom)
+
+        for i,j,btype in mmol.connectivity:
+            erdkmol.AddBond(i, j, Bond.orders[int(btype)])            
+
+        newmmol = erdkmol.GetMol()
+        conf = Chem.Conformer(len(mmol.geometry))
+        for i, coords in enumerate(mmol.geometry):
+            conf.SetAtomPosition(i, coords)
+        newmmol.AddConformer(conf)
+
+        return newmmol
