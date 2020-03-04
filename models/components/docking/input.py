904d8a1fa58515f75487edabc51ee289c453b6e6
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-04 18:06:05 -0500

    added Fields to all models

--- models/components/docking/input.py
+++ models/components/docking/input.py
@@ -6,16 +6,53 @@ from models.components.utils.input import FileInput
 from pydantic import Field
 
 class DockingInput(models.ProtoModel):
-    ligand: MMolecule = Field(..., description="Molecule model for candidate ligand (e.g. drug).")
-    receptor: MMolecule = Field(..., description="Molecule model for receptor (e.g. protein).")
-    searchSpace: Optional[List[Tuple[float, float, float, float, float, float]]] = Field(None, description="A 3D box defined by (xmin, xmax, ymin, ymax, zmin, zmax)."
-        "The search space effectively restricts where the movable atoms, including those in the flexible side chains, should lie.")
+    ligand: MMolecule = Field(
+        ..., 
+        description = "Molecule model for candidate ligand (e.g. drug)."
+    )
+    receptor: MMolecule = Field(
+        ..., 
+        description = "Molecule model for receptor (e.g. protein)."
+    )
+    searchSpace: Optional[List[Tuple[float, float, float, float, float, float]]] = Field(
+        None, 
+        description = "A 3D box defined by (xmin, xmax, ymin, ymax, zmin, zmax)."
+        "The search space effectively restricts where the movable atoms, including those in the flexible side chains, should lie."
+    )
 
 class DockingPrepInput(DockingInput):
-    ligand: Union[ChemCode, FileInput, MMolecule] = Field(..., description="Molecule model for candidate ligand (e.g. drug).")
-    receptor: Union[FileInput, MMolecule] = Field(..., description="Molecule model for receptor (e.g. protein).")
+    ligand: Union[ChemCode, FileInput, MMolecule] = Field(
+        ..., 
+        description = "Molecule model for candidate ligand (e.g. drug)."
+    )
+    receptor: Union[FileInput, MMolecule] = Field(
+        ..., 
+        description = "Molecule model for receptor (e.g. protein)."
+    )
 
 class DockingSimInput(models.ProtoModel):
-    dockingInput: DockingInput = Field(..., description="Docking input model.")
-    ligand: Union[List[str], List[MMolecule]] = Field(..., description="Molecule model for candidate ligand (e.g. drug).")
-    receptor: Union[str, MMolecule] = Field(..., description="Molecule model for receptor (e.g. protein).")
+    dockingInput: DockingInput = Field(
+        ..., 
+        description = "Docking input model."
+    )
+    ligand: str = Field(
+        ..., 
+        description = "Ligand file string."
+    )
+    receptor: str = Field(
+        ..., 
+        description = "Receptor file string."
+    )
+    cpu: Optional[int] = Field(
+        1, 
+        description = "The number of CPUs to use. The default is to try to "
+        "detect the number of CPUs."
+    )
+    out: Optional[str] = Field(
+        None, 
+        description = "Output models."
+    )
+    log: Optional[str] = Field(
+        None, 
+        description = "Log file output."
+    )
