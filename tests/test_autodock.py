c279babfef9be7c2d847b43bec1525a8efed91da
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-04 16:48:31 -0500

    synced test_autodock with latest changes

--- tests/test_autodock.py
+++ tests/test_autodock.py
@@ -2,45 +2,52 @@ import sys
 import numpy
 import os
 
-sys.path.insert(0, '..')
+sys.path.insert(0, os.getcwd())
+
+debug = False
 
 # import models
 from models.components.docking.input import DockingPrepInput
+from models.components.docking.autodock.input import AutoDockSimInput
 from models.components.utils.input import FileInput, OpenBabelInput, GrepInput
-from models.domains.classmech.molecule import ChemCode
-from models.domains.docking.molecule import MMolecule
+from models.tools.rdkit.codes import ChemCode
 
 # Test input file
-receptor = FileInput(path=os.path.abspath('../data/dialanine/dialanine.pdb'))
+receptor = FileInput(path=os.path.abspath('data/dialanine/dialanine.pdb'))
 ligand = ChemCode(code='CCC')
 
-docking_input_data = DockingPrepInput(ligand=ligand, receptor=receptor)
+dockingInputData = DockingPrepInput(ligand=ligand, receptor=receptor)
 
+# Import components
 from components.implementation.utils.openbabel_component import OpenBabel
 from components.implementation.utils.grep_component import Grep
+from components.implementation.docking.autodock_sim_component import AutoDockSim
 from components.implementation.docking.autodock_prep_component import AutoDockPrep
 from components.implementation.docking.autodock_convert_component import ConvertAutoDockComponent
 
-from config import TaskConfig
-
 # Test for ConvertAutoDockComponent
-docking_input = ConvertAutoDockComponent.compute(docking_input_data)
+dockingInput = ConvertAutoDockComponent.compute(dockingInputData)
 
 # Test for openbabel
 obabel_input = OpenBabelInput(fileInput=receptor, outputExt='pdbqt')
 obabel_output = OpenBabel.compute(obabel_input)
-print("==============================")
-print("OBABEL OUTPUT:")
-print("==============================")
-print(obabel_output.Contents)
+
+if debug:
+	print("==============================")
+	print("OBABEL OUTPUT:")
+	print("==============================")
+	print(obabel_output.Contents)
 
 # Test for grep
 grep_input = GrepInput(fileInput=receptor, pattern='ATOM')
 grep_output = Grep.compute(grep_input)
-print("==============================")
-print("GREP OUTPUT:")
-print("==============================")
-print(grep_output.Contents)
+
+if debug:
+	print("==============================")
+	print("GREP OUTPUT:")
+	print("==============================")
+	print(grep_output.Contents)
 
 # Test for AutodockPrep
-ADP = AutoDockPrep.compute(input_data=docking_input)
+simInput  = AutoDockPrep.compute(input_data=dockingInput)
+simOutput = AutoDockSim.compute(input_data=simInput)
