32cb78ba3cab30f0ff0459e8fcdb13d012e10d38
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-04 18:05:30 -0500

    completed DockingSimOutput definition

--- models/components/docking/output.py
+++ models/components/docking/output.py
@@ -1,19 +1,53 @@
 from typing import List, Optional, Tuple, Union
 from .input import DockingInput
 from models.domains.classmech.molecule import MMolecule
+from models.components.utils.output import CmdOutput 
+from models.components.utils.input import FileInput
+
 from qcelemental import models
 from pydantic import Field
 
 class DockingOutput(models.ProtoModel):
-    dockingInput: DockingInput = Field(..., description="Docking input model.")
-    poses: List[MMolecule] = Field(..., description="Conformation and orientation of the candidate ligand relative to the receptor.")
-    flexible: Optional[List[MMolecule]] = Field(None, description="Conformation and orientation of the flexible side chains in the receptor relative to the ligand.")
-    scores: Optional[List[float]] = Field(None, description="A metric for evaluating a particular pose. Length of scores must be equal to length of poses.")
+    dockingInput: DockingInput = Field(
+        ..., 
+        description = "Docking input model."
+    )
+    poses: List[MMolecule] = Field(
+        ...,
+        description = "Conformation and orientation of the candidate ligand relative to the receptor."
+    )
+    scores: Optional[List[float]] = Field(
+        ...,
+        description = "A metric for evaluating a particular pose. Length of scores must be equal to length of poses."
+    )
+    flexible: Optional[List[MMolecule]] = Field(
+        None,
+        description = "Conformation and orientation of the flexible side chains in the receptor relative to the ligand."
+    )
 
 class DockingSimOutput(models.ProtoModel):
-    dockingInput: DockingInput = Field(..., description="Docking input model.")
-    ligand: Union[MMolecule, str] = Field(..., description="Molecule model for candidate ligand (e.g. drug).")
-    receptor: Union[MMolecule, str] = Field(..., description="Molecule model for receptor (e.g. protein).")
-    poses: List[Union[MMolecule, str]] = Field(..., description="Conformation and orientation of the candidate ligand relative to the receptor.")
-    flexible: Optional[Union[MMolecule, str]] = Field(None, description="Conformation and orientation of the flexible side chains of the receptor relative to the ligand.")
-    scores: List[float] = Field(None, description="A metric for evaluating a particular pose. Length of scores must be equal to length of poses.")
+    dockingInput: DockingInput = Field(
+        ..., 
+        description = "Docking input model."
+    )
+    scores: List[float] = Field(
+        ..., 
+        description = "A metric for evaluating a particular pose. Length of scores must be equal to length of poses."
+    )
+    poses: Optional[List[FileInput]] = Field(
+        None, 
+        description = "Conformation and orientation of the candidate ligand relative to the receptor."
+    )
+    flexible: Optional[List[FileInput]] = Field(
+        None,
+        description = "Input file defining the conformation and orientation of the flexible side chains in the receptor "
+        "relative to the ligand."
+    )
+    system: Optional[str] = Field(
+        None,
+        description = "Input file string storing the ligand poses."
+    )
+    cmdout: Optional[CmdOutput] = Field(
+        None,
+        description = "Command-line output class which provides stdout, stderr, and log info."
+    )
