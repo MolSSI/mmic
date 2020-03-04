736f5f472c5147bfc0a69fc92e716c996703a5bb
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-04 12:01:40 -0500

    overwrote symbols + geometry fields in MMolecule; added docstring

--- models/domains/classmech/molecule.py
+++ models/domains/classmech/molecule.py
@@ -1,4 +1,5 @@
 import qcelemental
+from qcelemental.models.types import Array
 from typing import List, Tuple, Optional, Any, Dict, Union
 import os, sys
 import random
@@ -17,16 +18,60 @@ class Identifiers(qcelemental.models.molecule.Identifiers):
     helm: Optional[ChemCode] = None
 
 class MMolecule(qcelemental.models.Molecule):
+    """
+    An MMSchema representation of a Molecule based on QCSchema. This model contains data for symbols, geometry, 
+    connectivity, charges, residues, etc. while also supporting a wide array of I/O and manipulation capabilities.
+    Molecule objects geometry, masses, and charges are truncated to 8, 6, and 4 decimal places respectively 
+    to assist with duplicate detection.
+    """
 
-    rotateBonds: Optional[List[Tuple[int, int]]] = Field(None, description="A list of bonded atomic indices: (atom1, atom2), specifying rotatable bonds in the molecule.")
-    rigidBonds: Optional[List[Tuple[int, int]]] = Field(None, description="A list of bonded atomic indices: (atom1, atom2), specifying rigid bonds in the molecule.")
-    residues: Optional[List[Tuple[str, int]]] = Field(None, description="A list of (residue_name, residue_num) of connected atoms constituting the building block (monomer) of a polymer. Order "
-        " follows atomic indices from 0 till Natoms-1. E.g. ('ALA', 1) means atom 0 belongs to aminoacid alanine with residue number 1. Residue number >= 1.")
-    chains: Optional[Dict[str, List[int]]] = Field(None, description="A sequence of connected residues.")
-    segments: Optional[Dict[str, List[int]]] = Field(None, description="A 2D list of atomic indices specifying which bonds in the ligand are rotatable.")
-    identifiers: Optional[Identifiers] = Field(None, description="An optional dictionary of additional identifiers by which this MMolecule can be referenced, "
-        "such as INCHI, SMILES, SMARTs, etc. See the :class:``Identifiers`` model for more details.")
-    names: Optional[List[str]] = Field(None, description="A list of atomic label names.")
+    symbols: Array[str] = Field(
+        None,
+        description="An ordered (nat,) array-like object of atomic elemental symbols of shape (nat,). The index of "
+        "this attribute sets atomic order for all other per-atom setting like ``real`` and the first "
+        "dimension of ``geometry``. Ghost/Virtual atoms must have an entry in this array-like and are "
+        "indicated by the matching the 0-indexed indices in ``real`` field.",
+    )
+    geometry: Array[float] = Field( 
+        None,
+        description="An ordered (nat,3) array-like for XYZ atomic coordinates [a0]. "
+        "Atom ordering is fixed; that is, a consumer who shuffles atoms must not reattach the input "
+        "(pre-shuffling) molecule schema instance to any output (post-shuffling) per-atom results "
+        "(e.g., gradient). Index of the first dimension matches the 0-indexed indices of all other "
+        "per-atom settings like ``symbols`` and ``real``."
+        "\n"
+        "Can also accept array-likes which can be mapped to (nat,3) such as a 1-D list of length 3*nat, "
+        "or the serialized version of the array in (3*nat,) shape; all forms will be reshaped to "
+        "(nat,3) for this attribute.",
+    )
+    residues: Optional[List[Tuple[str, int]]] = Field(
+        None, 
+        description="A list of (residue_name, residue_num) of connected atoms constituting the building block (monomer) of a polymer. Order "
+        " follows atomic indices from 0 till Natoms-1. E.g. ('ALA', 1) means atom 0 belongs to aminoacid alanine with residue number 1. Residue number >= 1."
+        )
+    chains: Optional[Dict[str, List[int]]] = Field(
+        None, description="A sequence of connected residues."
+    )
+    segments: Optional[Dict[str, List[int]]] = Field(
+        None, 
+        description="A 2D list of atomic indices specifying which bonds in the ligand are rotatable."
+    )
+    identifiers: Optional[Identifiers] = Field(
+        None, 
+        description="An optional dictionary of additional identifiers by which this MMolecule can be referenced, "
+        "such as INCHI, SMILES, SMARTs, etc. See the :class:``Identifiers`` model for more details."
+    )
+    names: Optional[List[str]] = Field(
+        None, 
+        description="A list of atomic label names."
+    )
+    rotateBonds: Optional[List[Tuple[int, int]]] = Field(
+        None, 
+        description="A list of bonded atomic indices: (atom1, atom2), specifying rotatable bonds in the molecule."
+    )
+    rigidBonds: Optional[List[Tuple[int, int]]] = Field(
+        None, description="A list of bonded atomic indices: (atom1, atom2), specifying rigid bonds in the molecule."
+    )
 
     # Constructors
     @classmethod
