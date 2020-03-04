f0e8a8ac29a10fc83e0794f0bf921ef13fe7304a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-04 18:07:34 -0500

    completed definition of AutoDockSimInput

--- models/components/docking/autodock/input.py
+++ models/components/docking/autodock/input.py
@@ -1,12 +1,47 @@
 from typing import List, Optional, Tuple, Union
-from models.domains.classmech.molecule import MMolecule
+from models.components.docking.input import DockingSimInput
 from qcelemental import models 
+from pydantic import Field
 
-class AutoDockSimInput(models.ProtoModel):
-    ligand: str
-    receptor: str
-    exhaustiveness: Optional[int] = 8
-    seed: Optional[int] = None
-    num_modes: Optional[int] = 9
-    energy_range: Optional[int] = 3 # in kcal/mol
-    cpu: Optional[int] = None
+class AutoDockSimInput(DockingSimInput):
+    exhaustiveness: Optional[int]  = Field(
+        8, 
+        description = "Exhaustiveness of the global search (roughly proportional to time)"
+    )
+    seed: Optional[int] = Field(
+        None, 
+        description = "Random seed."
+    )
+    center_x: Optional[float] = Field(
+        None, 
+        description = "X coordinate of the search box center."
+    )
+    center_y: Optional[float] = Field(
+        None, 
+        description = "Y coordinate of the search box center."
+    )
+    center_z: Optional[float] = Field(
+        None, 
+        description = "Z coordinate of the search box center."
+    )
+    size_x: Optional[float] = Field(
+        None, 
+        description = "Search box size in the X dimension (Angstroms)."
+    )
+    size_y: Optional[float] = Field(
+        None, 
+        description = "Search box size in the Y dimension (Angstroms)."
+    )
+    size_z: Optional[float] = Field(
+        None, 
+        description = "Search box size in the Z dimension (Angstroms)."
+    )
+    num_modes: Optional[int] = Field(
+        9, 
+        description = "Maximum number of binding modes to generate."
+    )
+    energy_range: Optional[int] = Field(
+        3, 
+        description = "Maximum energy difference between the best binding mode "
+        "and the worst one displayed (kcal/mol)."
+    )
