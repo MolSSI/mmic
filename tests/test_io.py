11d7a56a5d8b74a7377a4935d8e47234131a733a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-04 16:48:23 -0500

    synced test_io with latest changes

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,20 +2,22 @@ import sys
 import numpy
 import os
 
-sys.path.insert(0, '..')
+sys.path.insert(0, os.getcwd())
+debug = False
 
 # import models
 from models.components.utils.input import FileInput
 from models.domains.classmech.molecule import MMolecule
 from models.tools.rdkit.codes import ChemCode
 
+
 # import converter component
 from components.implementation.utils.mm_reader import MMoleculeReader, MMoleculeReaderInput
 
 # Test input file
-pdbFile = FileInput(path=os.path.abspath('../data/dialanine/dialanine.pdb'))
+pdbFile = FileInput(path=os.path.abspath('data/dialanine/dialanine.pdb'))
 
-sdfFile = FileInput(path=os.path.abspath('../data/dummy_ligand/ligand.sdf'))
+sdfFile = FileInput(path=os.path.abspath('data/dummy_ligand/ligand.sdf'))
 smiles = ChemCode(code='CCC')
 
 #mmInput = MMoleculeReaderInput(code=smiles)
@@ -23,6 +25,25 @@ smiles = ChemCode(code='CCC')
 
 mol = MMolecule.from_file(filename=pdbFile.path)
 
-print("MMolecule info:")
-print("===============")
-print(mol.geometry)
+if debug:
+	print("MMolecule info:")
+	print("===============")
+
+	print("Bonds:")
+	print("======")
+	print(mol.connectivity)
+
+	print("Residues:")
+	print("==========")
+	print(mol.residues)
+
+	print("Positions:")
+	print("==========")
+	print(mol.geometry)
+
+	print("Atom Names:")
+	print("==========")
+	print(mol.atom_labels)
+
+
+mol.to_file('tmp.pdb')
