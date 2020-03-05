eacef002b3bf03ae9e444b81720c8f3657a406ba
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-05 16:24:16 -0500

    converter reads in raw input

--- components/implementation/docking/autodock_convert_component.py
+++ components/implementation/docking/autodock_convert_component.py
@@ -1,14 +1,17 @@
 from components.implementation.molecmech.mm_constructor import MMConstructorComponent
 from typing import Any, Dict, List, Optional, Tuple
+import os
 
-from models.components.docking.input import DockingInput, DockingPrepInput
+from models.components.docking.input import DockingInput, DockingRawInput
 from models.molecmech.molecules.mm_molecule import MMolecule
+from models.components.utils.input import FileInput
+from models.molecmech.chem.codes import ChemCode
 
 class ConvertAutoDockComponent(MMConstructorComponent):
 
     @classmethod
     def input(cls):
-        return DockingPrepInput
+        return DockingRawInput
 
     @classmethod
     def output(cls):
@@ -23,7 +26,16 @@ class ConvertAutoDockComponent(MMConstructorComponent):
         timeout: Optional[int] = None,
     ) -> Tuple[bool, Dict[str, Any]]:
 
-        ligand = self.constructor(inputs.ligand)
-        receptor = self.constructor(inputs.receptor)
+        if(os.path.isfile(inputs.ligand)):
+            ligand = FileInput(path=inputs.ligand)
+        else: # had better be a valid chem code
+            ligand = ChemCode(code=inputs.ligand)
+        ligand = self.constructor(ligand)
+
+        if(os.path.isfile(inputs.receptor)):
+            receptor = FileInput(path=inputs.receptor)
+        else: # had better be a valid chem code
+            receptor = ChemCode(code=inputs.receptor)
+        receptor = self.constructor(receptor)
 
         return True, DockingInput(ligand=ligand, receptor=receptor)
