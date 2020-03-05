70081d0c607bc303d46edf4d7f23608f393a9e84
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-05 11:39:53 -0500

    moved all rdkit-specific models to molecmech.molecules submodule

--- models/tools/rdkit/__init__.py
+++
@@ -1,8 +0,0 @@
-try:
-    from rdkit import rdBase, Chem
-    from rdkit.Chem import AllChem
-except:
-    raise ModuleNotFoundError('Make sure rdkit is installed for code validation.')
-
-from . import molecule
-from . import codes
