d277ca991c6282788dfe85979fb7d6f76a7c2a25
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-05 11:55:37 -0500

    fixed import paths to molecmech submodules

--- models/components/docking/autodock/output.py
+++ models/components/docking/autodock/output.py
@@ -1,6 +1,6 @@
 from ..input import DockingInput
 from ..output import DockingSimOutput
-from models.domains.classmech.molecule import MMolecule
+from models.molecmech.molecules.mm_molecule import MMolecule
 
 from qcelemental import models
 from typing import List, Optional, Tuple, Union
