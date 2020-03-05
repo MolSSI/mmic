8710894fa7b0f8e06d6ecca90a71c7064a1cc301
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-05 11:54:21 -0500

    added rec/lig prep helper method

--- components/blueprints/docking/docking_sim_prep_component.py
+++ components/blueprints/docking/docking_sim_prep_component.py
@@ -1,12 +1,13 @@
 import sys
 import abc
-sys.path.insert(0, '..')
 
 from components.base.base_component import ProgramHarness
 from models.components.docking.input import DockingInput, DockingSimInput
+from models.molecmech.molecules.mm_molecule import MMolecule
 
 from typing import Any, Dict, List, Optional, Tuple
-
+import random
+import string
 
 class DockSimPrepComponent(ProgramHarness, abc.ABC):
 
@@ -46,7 +47,19 @@ class DockSimPrepComponent(ProgramHarness, abc.ABC):
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
     ) -> Tuple[bool, Dict[str, Any]]:
-        raise ValueError("execute is not implemented for {}.", self.__class__)
+        raise NotImplementedError("execute is not implemented for {}.", self.__class__)
 
     def parse_output(self, outfiles: Dict[str, str], input_model: "DockingInput") -> "DockPrepOutput":
-        raise ValueError("parse_output is not implemented for {}.", self.__class__)  
+        raise NotImplementedError("parse_output is not implemented for {}.", self.__class__)
+
+    # helper functions
+    def receptor_prep(self, receptor: MMolecule) -> Any:
+        raise NotImplementedError(f"receptor_prep is not implemented for {self.__class__}.")
+
+    def ligand_prep(self, receptor: MMolecule) -> Any:
+        raise NotImplementedError(f"ligand_prep is not implemented for {self.__class__}.")
+
+    @staticmethod
+    def randomString(stringLength=10) -> str:
+       letters = string.ascii_lowercase
+       return ''.join(random.choice(letters) for i in range(stringLength))
