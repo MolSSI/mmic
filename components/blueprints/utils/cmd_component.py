0f38f2071fe895e8cb0a4560a86a6f9f1de411d1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-05 16:15:08 -0500

    make CmdComponent more generic: added clean and execute methods

--- components/blueprints/utils/cmd_component.py
+++ components/blueprints/utils/cmd_component.py
@@ -1,8 +1,8 @@
 from qcengine.util import execute
 from qcelemental import models
+from models.components.utils.output import FileOutput
 from components.base.base_component import ProgramHarness
-from typing import Any, Dict, List, Optional, Tuple
-from qcelemental import models
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 class CmdComponent(ProgramHarness):
 
@@ -14,13 +14,38 @@ class CmdComponent(ProgramHarness):
     def output(cls):
         return models.ProtoModel
 
+    def clean(self, files: Union[List[FileOutput], FileOutput]):
+        if isinstance(files, list):
+            for file in files:
+                file.remove()
+        else:
+            files.remove()
+
+    def execute(self,
+        inputs: Dict[str, Any],
+        extra_outfiles: Optional[List[str]] = None,
+        extra_commands: Optional[List[str]] = None,
+        scratch_name: Optional[str] = None,
+        timeout: Optional[int] = None
+    ) -> Tuple[bool, Dict[str, Any]]:
+
+        execute_input = self.build_input(inputs)
+        exe_success, proc = self.run(execute_input, clean_files=execute_input.get('clean_files'))
+
+        if exe_success:
+            return True, self.parse_output(proc, inputs)
+        else:
+            raise ValueError(proc["stderr"])
+
     def run(
         self,
         inputs: Dict[str, Any],
         extra_outfiles: Optional[List[str]] = None,
         extra_commands: Optional[List[str]] = None,
         scratch_name: Optional[str] = None,
-        timeout: Optional[int] = None) -> Tuple[bool, Dict[str, Any]]:
+        timeout: Optional[int] = None,
+        clean_files: Optional[Union[List[FileOutput], FileOutput]] = None
+    ) -> Tuple[bool, Dict[str, Any]]:
 
         infiles = inputs["infiles"]
 
@@ -41,5 +66,8 @@ class CmdComponent(ProgramHarness):
             timeout=timeout,
             environment=inputs.get("environment", None)
         )
+
+        if clean_files:
+            self.clean(clean_files)
         
         return exe_success, proc
