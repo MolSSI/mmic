477a841a7e8767bb0e5da0576874a716d051ac4a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-08 23:45:12 -0400

    replaced unused methods receptor/ligand_prep() with get_scores()

--- components/blueprints/docking/docking_post_component.py
+++ components/blueprints/docking/docking_post_component.py
@@ -1,12 +1,9 @@
 from components.base.base_component import ProgramHarness
 from models.components.docking.output import DockingOutput, DockingSimOutput
-from models.molecmech.molecules.mm_molecule import MMolecule
-
-from typing import Any
-import random
-import string
+from typing import Any, List
 
 class DockPostComponent(ProgramHarness):
+    """ Postprocessing component for docking. """
 
     @classmethod
     def input(cls):
@@ -17,8 +14,5 @@ class DockPostComponent(ProgramHarness):
         return DockingOutput
 
     # helper functions
-    def receptor_prep(self, receptor: MMolecule) -> Any:
-        raise NotImplementedError(f"receptor_prep is not implemented for {self.__class__}.")
-
-    def ligand_prep(self, receptor: MMolecule) -> Any:
-        raise NotImplementedError(f"ligand_prep is not implemented for {self.__class__}.")
+    def get_scores(self, output: Any) -> List[float]:
+        raise NotImplementedError("get_scores is not implemented for {}.", self.__class__)
