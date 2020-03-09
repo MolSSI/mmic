ea31e0c5fac9691b088a3d30bb15285ee399bf26
Andrew-AbiMansour <andrew.gaam@gmail.com>
2020-03-09 03:17:18 -0400

    renamed DockingSimOutput to DockingComputeOutput

--- models/components/docking/autodock/output.py
+++ models/components/docking/autodock/output.py
@@ -1,10 +1,10 @@
 from ..input import DockingInput
-from ..output import DockingSimOutput
+from ..output import DockingComputeOutput
 from models.molecmech.molecules.mm_molecule import MMolecule
 
 from qcelemental import models
 from typing import List, Optional, Tuple, Union
 from pydantic import Field
 
-class AutoDockSimOutput(DockingSimOutput):
+class AutoDockComputeOutput(DockingComputeOutput):
     pass
