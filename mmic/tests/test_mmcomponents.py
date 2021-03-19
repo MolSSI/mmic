8154d4a18fdfb2f4c5e07ef135a20318910162db
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-19 15:53:59 -0400

    add basic tests for pytest

--- mmic/tests/test_mmcomponents.py
+++ mmic/tests/test_mmcomponents.py
@@ -11,3 +11,25 @@ import sys
 def test_mmic_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "mmic" in sys.modules
+
+
+def test_mmic_generic():
+    class Foo(mmic.components.blueprints.generic_component.GenericComponent):
+        def execute(self):
+            pass
+
+    f = Foo(
+        name="foo",
+        scratch=False,
+        thread_safe=False,
+        thread_parallel=False,
+        node_parallel=False,
+        managed_memory=False,
+        extras=None,
+    )
+
+
+def test_mmic_validation():
+    class Foo(mmic.components.blueprints.generic_component.GenericComponent):
+        def execute(self, inputs):
+            return True, self.output()()
