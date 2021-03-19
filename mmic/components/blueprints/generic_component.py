862a72fc8e3430822dff9929da25ecf4f654cb7a
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-19 15:53:44 -0400

    add found and get_version methods

--- mmic/components/blueprints/generic_component.py
+++ mmic/components/blueprints/generic_component.py
@@ -1,8 +1,7 @@
 from mmic.components.base.base_component import ProgramHarness
-from qcelemental import models
+
 
 class GenericComponent(ProgramHarness):
-
     @classmethod
     def input(cls):
         return models.ProtoModel
@@ -10,3 +9,27 @@ class GenericComponent(ProgramHarness):
     @classmethod
     def output(cls):
         return models.ProtoModel
+
+    def get_version(self) -> str:
+        """Finds program, extracts version, returns normalized version string.
+        Returns
+        -------
+        str
+            Return a valid, safe python version string.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def found(raise_error: bool = False) -> bool:
+        """
+        Checks if the program can be found.
+        Parameters
+        ----------
+        raise_error : bool, optional
+            If True, raises an error if the program cannot be found.
+        Returns
+        -------
+        bool
+            Returns True if the program was found, False otherwise.
+        """
+        ...
