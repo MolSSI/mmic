981f273532b6b5ae47d00929273e2c80ae77020f
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-19 15:53:14 -0400

    add additional kwargs to cls.compute, update code format

--- mmic/components/base/base_component.py
+++ mmic/components/base/base_component.py
@@ -4,38 +4,18 @@ from .config import TaskConfig
 from qcelemental import models
 from pydantic import Field
 
+
 class ProgramHarness(models.ProtoModel, abc.ABC):
 
-    name: str = Field(
-        ..., 
-        description='...'
-    )
-    scratch: bool = Field(
-        ..., 
-        description='...'
-    )
-    thread_safe: bool = Field(
-        ..., 
-        description='...'
-    )
-    thread_parallel: bool = Field(
-        ..., 
-        description='...'
-    )
-    node_parallel: bool = Field(
-        ..., 
-        description='...'
-    )
-    managed_memory: bool = Field(
-        ..., 
-        description='...'
-    )
-    extras: Optional[Dict[str, Any]] = Field(
-        None, 
-        description='...'
-    )
-    _defaults: Dict[str, Any] =  {}
-    
+    _defaults: Dict[str, Any] = {}
+    name: str
+    scratch: bool
+    thread_safe: bool
+    thread_parallel: bool
+    node_parallel: bool
+    managed_memory: bool
+    extras: Optional[Dict[str, Any]]
+
     class Config:
         allow_mutation = False
         extra = "forbid"
@@ -52,32 +32,55 @@ class ProgramHarness(models.ProtoModel, abc.ABC):
         return models.ProtoModel
 
     @classmethod
-    def compute(cls, input_data: models.ProtoModel, config: TaskConfig = None) -> models.ProtoModel:
-        
-        #Validate the input model
+    def compute(
+        cls,
+        input_data: models.ProtoModel,
+        config: Optional[TaskConfig] = None,
+        scratch: Optional[bool] = False,
+        thread_safe: Optional[bool] = False,
+        thread_parallel: Optional[bool] = False,
+        node_parallel: Optional[bool] = False,
+        managed_memory: Optional[bool] = False,
+        extras: Optional[Dict[str, Any]] = None,
+    ) -> models.ProtoModel:
+
+        # Validate the input model
         if isinstance(input_data, cls.input()):
             cls.input()(**input_data.dict())
         elif isinstance(input_data, dict):
             cls.input()(**input_data)
         else:
-            raise TypeError("{0} is not a valid input type for the {1} component.".format(type(input_data), cls.__name__))
+            raise TypeError(
+                f"{type(input_data)} is not a valid input type for the {cls.__name__} component."
+            )
 
-        #Perform the execution
-        program = cls(name = cls.__name__, scratch = False, thread_safe = False, thread_parallel = False, node_parallel = False, managed_memory = False)
+        # Perform the execution
+        program = cls(
+            name=cls.__name__,
+            scratch=scratch,
+            thread_safe=thread_safe,
+            thread_parallel=thread_parallel,
+            node_parallel=node_parallel,
+            managed_memory=managed_memory,
+            extras=extras,
+        )
 
         _, exec_output = program.execute(input_data)
 
-        #Validate the output model
+        # Validate the output model
         if isinstance(exec_output, cls.output()):
             cls.output()(**exec_output.dict())
         elif isinstance(exec_output, dict):
             cls.output()(**exec_output)
         else:
-            raise TypeError("{0} is not a valid output type for the {1} component.".format(type(exec_output), cls.__name__))
+            raise TypeError(
+                f"{type(exec_output)} is not a valid output type for the {cls.__name__} component."
+            )
 
         return exec_output
 
     @staticmethod
+    @abc.abstractmethod
     def found(raise_error: bool = False) -> bool:
         """
         Checks if the program can be found.
@@ -93,6 +96,7 @@ class ProgramHarness(models.ProtoModel, abc.ABC):
 
     ## Utility
 
+    @abc.abstractmethod
     def get_version(self) -> str:
         """Finds program, extracts version, returns normalized version string.
         Returns
@@ -100,13 +104,20 @@ class ProgramHarness(models.ProtoModel, abc.ABC):
         str
             Return a valid, safe python version string.
         """
+        pass
 
     ## Computers
     def build_input(
-        self, input_model: models.ProtoModel, config: TaskConfig = None, template: Optional[str] = None
+        self,
+        input_model: models.ProtoModel,
+        config: TaskConfig = None,
+        template: Optional[str] = None,
     ) -> Dict[str, Any]:
-        raise NotImplementedError("build_input is not implemented for {}.", self.__class__)
+        raise NotImplementedError(
+            f"build_input is not implemented for {self.__class__}."
+        )
 
+    @abc.abstractmethod
     def execute(
         self,
         inputs: Dict[str, Any],
@@ -115,7 +126,11 @@ class ProgramHarness(models.ProtoModel, abc.ABC):
         scratch_name: Optional[str] = None,
         timeout: Optional[int] = None,
     ) -> Tuple[bool, Dict[str, Any]]:
-        raise NotImplementedError("execute is not implemented for {}.", self.__class__)
+        raise NotImplementedError(f"execute is not implemented for {self.__class__}.")
 
-    def parse_output(self, outfiles: Dict[str, str], input_model: models.ProtoModel) -> models.ProtoModel:
-        raise NotImplementedError("parse_output is not implemented for {}.", self.__class__)
+    def parse_output(
+        self, outfiles: Dict[str, str], input_model: models.ProtoModel
+    ) -> models.ProtoModel:
+        raise NotImplementedError(
+            f"parse_output is not implemented for {self.__class__}."
+        )
