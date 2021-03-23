910e6fcaa847ed846f7770fbc16b2b8904319a73
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-23 12:57:30 -0400

    add SpecificComponent

---
+++ mmic/components/blueprints/specific_component.py
@@ -0,0 +1,39 @@
+from ..base.base_component import ProgramHarness
+from qcelemental import models
+
+
+__all__ = ["SpecificComponent"]
+
+
+class SpecificComponent(ProgramHarness):
+    @classmethod
+    def input(cls):
+        return models.ProtoModel
+
+    @classmethod
+    def output(cls):
+        return models.ProtoModel
+
+    def get_version(self) -> str:
+        """Finds program, extracts version, returns normalized version string.
+        Returns
+        -------
+        str
+            Return a valid, safe python version string.
+        """
+        raise NotImplementedError
+
+    @staticmethod
+    def found(raise_error: bool = False) -> bool:
+        """
+        Checks if the program can be found.
+        Parameters
+        ----------
+        raise_error : bool, optional
+            If True, raises an error if the program cannot be found.
+        Returns
+        -------
+        bool
+            Returns True if the program was found, False otherwise.
+        """
+        raise NotImplementedError
