609e8380d810d2b6c04a302b69270e2d212dcf3c
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-04-21 15:17:37 -0400

    update imports

--- mmic/components/blueprints/__init__.py
+++ mmic/components/blueprints/__init__.py
@@ -1,5 +1,7 @@
 from . import generic_component
-from . import specific_component
+from . import strategy_component
+from . import tactic_component
 
 from .generic_component import *
-from .specific_component import *
+from .strategy_component import *
+from .tactic_component import *
